83aba3ec98c69ffec74109befc2c79f335cd49cb
Dmitry Zolotyh <zolotyh.dk@yandex.ru>
2024-03-18 20:54:50 +0300

    Упростить демонстрационный сценарий в Main (как в примере Практикума)

--- src/ru/yandex/javacourse/zolotyh/schedule/Main.java
+++ src/ru/yandex/javacourse/zolotyh/schedule/Main.java
@@ -11,206 +11,70 @@ import ru.yandex.javacourse.zolotyh.schedule.task.Task;
 import ru.yandex.javacourse.zolotyh.schedule.util.Managers;
 
 public class Main {
-    private static final String DELIMITER = "-".repeat(150);
+    private static final String DELIMITER = "-".repeat(150) + '\n';
+    private static final TaskManager taskManager = Managers.getDefault();
+    private static final HistoryManager historyManager = taskManager.getHistoryManager();
 
     public static void main(String[] args) {
-        TaskManager taskManager = Managers.getDefault();
-
-        Task task1 = new Task(null,
-                "Выучить инкапсуляцию.",
-                "Разобраться с модификаторами доступа.",
-                Status.NEW);
-        addNewTaskTest(task1, taskManager);
-
-        Task task2 = new Task(null,
-                "Выучить наследование.",
-                "Разобраться с ключевым словом extends и переопределением методов.",
-                Status.DONE);
-        updateTaskTest(task2, taskManager);
-
-        Epic epic1 = new Epic(null,
-                "Приложение канбан-доска",
-                "Разработать приложение канбан-доску");
-        addNewEpicTest(epic1, taskManager);
-
-        Subtask subtask1 = new Subtask(null,
-                "Разработать бэкенд",
-                "Написть серверную часть приложения",
-                Status.IN_PROGRESS,
-                epic1.getId());
-        addNewSubtaskTest(subtask1, taskManager);
-
-        Subtask subtask2 = new Subtask(null,
-                "Разработать фронтенд",
-                "Написать клиентскую часть приложения",
-                Status.DONE,
-                epic1.getId());
-        addNewSubtaskTest(subtask2, taskManager);
-
-        Epic epic2 = new Epic(null,
-                "iOS приложение",
-                "Разработать iOS приложение для канбан-доски");
-        updateEpicTest(epic2, taskManager);
-        Subtask subtask3 = new Subtask(null,
-                "Выучить swift",
-                "Без swift нам никак не написать приложение для iOS",
-                Status.NEW,
-                epic2.getId());
-        updateSubtaskTest(subtask3, taskManager);
-
-        getSubtasksByEpicTest(epic1, taskManager);
-
-        getAllTasksTest(taskManager);
-        getAllEpicsTest(taskManager);
-        getAllSubtasksTest(taskManager);
-
-        getHistoryCheck(taskManager);
-
-        deleteTaskTest(task1.getId(), taskManager);
-        deleteSubtaskTest(subtask1.getId(), taskManager);
-        deleteEpicTest(epic1.getId(), taskManager);
-
-        deleteAllTasksTest(taskManager);
-        deleteAllSubtasksTest(taskManager);
-        deleteAllEpicsTest(taskManager);
+        initialiseRepository();
+        printAllTasks();
+        showHistory();
     }
 
-    private static void addNewTaskTest(Task task, TaskManager manager) {
-        System.out.println("addNewTaskTest");
-        int id = manager.addNewTask(task);
-        System.out.println("Сохранено: " + manager.getTaskById(id));
+    public static void initialiseRepository() {
+        Epic epic = new Epic(null, "Эпик", "Описание эпика");
+        taskManager.addNewEpic(epic);
+        Subtask subtask1 = new Subtask(null, "Подзадача 1", "Описание подзадачи 1", Status.IN_PROGRESS, epic.getId());
+        Subtask subtask2 = new Subtask(null, "Подзадача 2", "Описание подзадачи 2", Status.DONE, epic.getId());
+        Subtask subtask3 = new Subtask(null, "Подзадача 3", "Описание подзадачи 3", Status.NEW, epic.getId());
+        taskManager.addNewSubtask(subtask1);
+        taskManager.addNewSubtask(subtask2);
+        taskManager.addNewSubtask(subtask3);
+        Task task1 = new Task(null, "Задача 1", "Описание задачи 1", Status.NEW);
+        Task task2 = new Task(null, "Задача 2", "Описание задачи 2", Status.DONE);
+        taskManager.addNewTask(task1);
+        taskManager.addNewTask(task2);
+    }
+
+    private static void printAllTasks() {
+        System.out.println("Задачи:");
+        for (Task task : taskManager.getAllTasks()) {
+            System.out.println(task);
+        }
+        System.out.println("Эпики:");
+        for (Epic epic : taskManager.getAllEpics()) {
+            System.out.println(epic);
+
+            for (Task task : taskManager.getSubtasksByEpic(epic)) {
+                System.out.println("--> " + task);
+            }
+        }
+        System.out.println("Подзадачи:");
+        for (Task subtask : taskManager.getAllSubtasks()) {
+            System.out.println(subtask);
+        }
         System.out.println(DELIMITER);
     }
 
-    private static void updateTaskTest(Task task, TaskManager manager) {
-        System.out.println("updateTaskTest");
-        int id = manager.addNewTask(task);
-        Task updated = new Task(id,
-                task.getName(),
-                task.getDescription() + " А ещё с сокрытием.",
-                Status.DONE);
-        manager.updateTask(updated);
-        System.out.println("Обновлено: " + manager.getTaskById(id));
+    private static void showHistory() {
+        System.out.println("Просмотрим задачу:");
+        int taskId = taskManager.getAllTasks().get(0).getId();
+        System.out.println(taskManager.getTaskById(taskId));
         System.out.println(DELIMITER);
-    }
 
-    private static void addNewEpicTest(Epic epic, TaskManager manager) {
-        System.out.println("addNewEpicTest");
-        int id = manager.addNewEpic(epic);
-        System.out.println("Сохранено: " + manager.getEpicById(id));
+        System.out.println("Просмотрим эпик:");
+        int epicId = taskManager.getAllEpics().get(0).getId();
+        System.out.println(taskManager.getEpicById(epicId));
         System.out.println(DELIMITER);
-    }
 
-    private static void addNewSubtaskTest(Subtask subtask, TaskManager manager) {
-        System.out.println("addNewSubtaskTest");
-        int id = manager.addNewSubtask(subtask);
-        System.out.println("Сохранено: " + manager.getSubtaskById(id));
-        System.out.println("Статус эпика теперь: " + manager.getEpicById(subtask.getEpicId()).getStatus());
+        System.out.println("Просмотрим подзадачу:");
+        int subtaskId = taskManager.getAllSubtasks().get(0).getId();
+        System.out.println(taskManager.getSubtaskById(subtaskId));
         System.out.println(DELIMITER);
-    }
 
-    private static void updateEpicTest(Epic epic, TaskManager manager) {
-        System.out.println("updateEpicTest");
-        int id = manager.addNewEpic(epic);
-        Epic updated = new Epic(id, "Kanban-board iOS application ", epic.getDescription());
-        manager.updateEpic(updated);
-        System.out.println("Обновлено: " + manager.getEpicById(id));
-        System.out.println(DELIMITER);
-    }
-
-    private static void updateSubtaskTest(Subtask subtask, TaskManager manager) {
-        System.out.println("updateSubtaskTest");
-        manager.addNewSubtask(subtask);
-        int id = subtask.getId();
-        Subtask updated = new Subtask(id, subtask.getName(), subtask.getDescription(), Status.DONE, subtask.getEpicId());
-        manager.updateSubtask(updated);
-        System.out.println("Обновлено: " + manager.getSubtaskById(id));
-        System.out.println("Статус эпика теперь: " + manager.getEpicById(subtask.getEpicId()).getStatus());
-        System.out.println(DELIMITER);
-    }
-
-    public static void getSubtasksByEpicTest(Epic epic, TaskManager manager) {
-        System.out.println("getSubtasksByEpicTest");
-        System.out.println("Для эпика: " + epic);
-        System.out.println("все подзадачи: ");
-        manager.getSubtasksByEpic(epic).forEach(System.out::println);
-        System.out.println(DELIMITER);
-    }
-
-    private static void getAllTasksTest(TaskManager manager) {
-        System.out.println("getAllTasksTest()");
-        manager.getAllTasks().forEach(System.out::println);
-        System.out.println(DELIMITER);
-    }
-
-    private static void getAllEpicsTest(TaskManager manager) {
-        System.out.println("getAllEpicsTest");
-        manager.getAllEpics().forEach(System.out::println);
-        System.out.println(DELIMITER);
-    }
-
-    public static void getAllSubtasksTest(TaskManager manager) {
-        System.out.println("getAllSubtasksTest");
-        manager.getAllSubtasks().forEach(System.out::println);
-        System.out.println(DELIMITER);
-    }
-
-    public static void getHistoryCheck(TaskManager manager) {
-        System.out.println("getHistoryCheck");
-        InMemoryTaskManager inMemoryTaskManager = (InMemoryTaskManager) manager;
-        inMemoryTaskManager.getHistoryManager().getHistory().forEach(System.out::println);
-        System.out.println(DELIMITER);
-    }
-
-    public static void deleteTaskTest(int id, TaskManager manager) {
-        System.out.println("deleteTaskTest");
-        manager.deleteTask(id);
-        System.out.println("После удаления задачи: ");
-        manager.getAllTasks().forEach(System.out::println);
-        System.out.println(DELIMITER);
-    }
-
-    public static void deleteSubtaskTest(int id, TaskManager manager) {
-        System.out.println("deleteSubtaskTest");
-        manager.deleteSubtask(id);
-        System.out.println("Все подзадачи после удаления:");
-        manager.getAllSubtasks().forEach(System.out::println);
-        System.out.println("Все эпики после удаления подзадачи:");
-        manager.getAllEpics().forEach(System.out::println);
-        System.out.println(DELIMITER);
-    }
-
-    public static void deleteEpicTest(int id, TaskManager manager) {
-        System.out.println("deleteEpicTest");
-        manager.deleteEpic(id);
-        System.out.println("Все эпики после удаления:");
-        manager.getAllEpics().forEach(System.out::println);
-        System.out.println("Все подзадачи после удаления эпика:");
-        manager.getAllSubtasks().forEach(System.out::println);
-        System.out.println(DELIMITER);
-    }
-
-    public static void deleteAllTasksTest(TaskManager manager) {
-        System.out.println("deleteAllTasksTest");
-        manager.deleteAllTasks();
-        System.out.println("Удалили все задачи. Задач осталось: " + manager.getAllTasks().size());
-        System.out.println(DELIMITER);
-    }
-
-    public static void deleteAllSubtasksTest(TaskManager manager) {
-        System.out.println("deleteAllSubtasksTest");
-        manager.deleteAllSubtasks();
-        System.out.println("Удалили все позадачи. Подзадач осталось: " + manager.getAllSubtasks().size());
-        System.out.println("Все эпики после удаления подзадач:");
-        manager.getAllEpics().forEach(System.out::println);
-        System.out.println(DELIMITER);
-    }
-
-    public static void deleteAllEpicsTest(TaskManager manager) {
-        System.out.println("deleteAllEpicsTest");
-        manager.deleteAllEpics();
-        System.out.println("Удалили все эпики. Эпиков осталось: " + manager.getAllEpics().size());
-        System.out.println("Подзадач осталось: " + manager.getAllSubtasks().size());
-        System.out.println(DELIMITER);
+        System.out.println("История просмотров:");
+        for (Task task : historyManager.getHistory()) {
+            System.out.println(task);
+        }
     }
 }
