1731c0246100621f8e554828d080139951696f7b
Dmitry Zolotyh <zolotyh.dk@yandex.ru>
2024-03-19 22:33:12 +0300

    Заменить ArrayList на LinkedList в InMemoryHistoryManager, вынести максимальный размер листа в константу

--- src/main/ru/yandex/javacourse/zolotyh/schedule/manager/InMemoryHistoryManager.java
+++ src/main/ru/yandex/javacourse/zolotyh/schedule/manager/InMemoryHistoryManager.java
@@ -3,14 +3,18 @@ package ru.yandex.javacourse.zolotyh.schedule.manager;
 import ru.yandex.javacourse.zolotyh.schedule.task.Task;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class InMemoryHistoryManager implements HistoryManager {
-    private final ArrayList<Task> history = new ArrayList<>(10);
+    /*Поменял реализацию на LinkedList из-за лучшей временной сложности удаления первого элемента
+    * https://stackoverflow.com/a/30633356*/
+    private final List<Task> history = new LinkedList<>();
+    private final static int HISTORY_SIZE = 10;
 
     @Override
     public void add(Task task) {
-        if (history.size() >= 10) {
+        if (history.size() >= HISTORY_SIZE) {
             history.remove(0);
         }
         history.add(task);
